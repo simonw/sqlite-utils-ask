69027751c1438a0c09c9dd4c79d50db0b55a31c8
Simon Willison <swillison@gmail.com>
2024-08-16 21:56:05 -0700

    Initial prototype, refs #1

---
+++ README.md
@@ -0,0 +1,35 @@
+# sqlite-utils-ask
+
+[![PyPI](https://img.shields.io/pypi/v/sqlite-utils-ask.svg)](https://pypi.org/project/sqlite-utils-ask/)
+[![Changelog](https://img.shields.io/github/v/release/simonw/sqlite-utils-ask?include_prereleases&label=changelog)](https://github.com/simonw/sqlite-utils-ask/releases)
+[![Tests](https://github.com/simonw/sqlite-utils-ask/workflows/Test/badge.svg)](https://github.com/simonw/sqlite-utils-ask/actions?query=workflow%3ATest)
+[![License](https://img.shields.io/badge/license-Apache%202.0-blue.svg)](https://github.com/simonw/sqlite-utils-ask/blob/main/LICENSE)
+
+Ask questions of your data with LLM assistance
+
+## Installation
+
+Install this plugin in the same environment as sqlite-utils.
+```bash
+sqlite-utils install sqlite-utils-ask
+```
+## Usage
+
+Usage instructions go here.
+
+## Development
+
+To set up this plugin locally, first checkout the code. Then create a new virtual environment:
+```bash
+cd sqlite-utils-ask
+python -m venv venv
+source venv/bin/activate
+```
+Now install the dependencies and test dependencies:
+```bash
+pip install -e '.[test]'
+```
+To run the tests:
+```bash
+python -m pytest
+```
---
+++ sqlite_utils_ask.py
@@ -0,0 +1,55 @@
+import click
+import json
+import re
+import sqlite_utils
+import textwrap
+
+
+@sqlite_utils.hookimpl
+def register_commands(cli):
+    import llm
+
+    @cli.command()
+    @click.argument(
+        "path",
+        type=click.Path(exists=True, file_okay=True, dir_okay=False, allow_dash=False),
+    )
+    @click.argument("question")
+    @click.option("model_id", "-m", "--model", help="LLM model to use")
+    def ask(path, question, model_id):
+        "Ask a question of your data"
+        db = sqlite_utils.Database(path)
+        schema = db.schema
+        system = textwrap.dedent(
+            """
+        You will be given a SQLite schema followed by a question. Generate a SQL
+        query to answer that question. Return that query in a ```sql ... ```
+        fenced code block.
+                                 
+        Example: How many repos are there?
+        Answer:
+        ```sql
+        select count(*) from repos
+        ```
+        """
+        )
+        if not model_id:
+            model_id = "gpt-4o-mini"
+        model = llm.get_model(model_id)
+        response = model.prompt(schema + "\n\n" + question, system=system)
+        sql = extract_sql_query(response.text())
+        if not sql:
+            raise click.ClickException(
+                "Failed to generate a response:\n\n" + response.text()
+            )
+        results = list(db.query(sql))
+        print(json.dumps({"sql": sql, "results": results}, indent=4, default=repr))
+
+
+def extract_sql_query(text):
+    pattern = r"```sql\n(.*?)\n```"
+    match = re.search(pattern, text, re.DOTALL)
+    if match:
+        return match.group(1).strip()
+    else:
+        return None
