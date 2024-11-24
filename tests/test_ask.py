9e849609aab0ff8aff6a9843c13b0e17f8b56fc7
Simon Willison <swillison@gmail.com>
2024-11-24 12:15:17 -0800

    Initial test for ask command

--- tests/test_ask.py
+++ tests/test_ask.py
@@ -1,7 +1,25 @@
-from sqlite_utils.plugins import get_plugins
+import sqlite_utils
+from sqlite_utils.cli import cli
+from click.testing import CliRunner
+import pytest
 
 
-def test_plugin_is_installed():
-    plugins = get_plugins()
-    names = [plugin["name"] for plugin in plugins]
-    assert "sqlite-utils-ask" in names
+@pytest.fixture(scope="module")
+def vcr_config():
+    return {"filter_headers": ["authorization"]}
+
+
+@pytest.mark.vcr
+def test_ask():
+    with CliRunner().isolated_filesystem():
+        db = sqlite_utils.Database("test.db")
+        db["dogs"].insert_all(
+            [
+                {"name": "Cleo"},
+                {"name": "Pancakes"},
+                {"name": "Jasper"},
+            ]
+        )
+        result = CliRunner().invoke(cli, ["ask", "test.db", "count the dogs"])
+        assert result.exit_code == 0
+        assert "3" in result.output
